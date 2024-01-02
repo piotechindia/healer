a4e5e72d73ccb9f7acd76719b88e9bc3dbe49eb9
Gaurav Sharma <104793185+gauravsharma8155@users.noreply.github.com>
2024-01-02 17:16:24 +0000

    modified:   app/login/page.tsx modified:   app/signup/page.tsx modified:   app/signup/signup.module.css

---
+++ app/login/page.tsx
@@ -0,0 +1,63 @@
+
+function Login() {
+  
+  return (
+    <div className='signin-main-container'>
+      <div className='signin-container'>
+        <div className='signin-box-1'>
+          <div className='signin-heading-div'>
+            <div className='signin-heading'>
+              Hello, Friend!
+            </div>
+          </div>
+
+          <div className='signin-text-div'>
+            <div className='signin-text'>
+              Enter your details and start journey with us
+            </div>
+          </div>
+
+          <div className='signin-btn-div'>
+            <button type='button'  className='signin-btn'>SIGN UP</button>
+          </div>
+        </div>
+        <div className='signin-box-2'>
+          <div className='signin-box-2-heading-div'>
+            <div className='signin-box-2-heading'>
+              Sign in
+            </div>
+          </div>
+
+          <div className='signin-box-2-formdiv'>
+            <div className='signin-email-box'>
+              <div className='signin-email'>
+                Email Address
+              </div>
+            </div>
+            <div className='signin-email_address'>
+              <input type='email' name="email" className='signin-email_address_box'  />
+            </div>
+
+
+            <div className='signin-email-box'>
+              <div className='signin-email'>
+                Password
+              </div>
+            </div>
+            <div className='signin-email_address'>
+              <input type='Password' name="password" className='signin-email_address_box' />
+            </div>
+
+            <div className='signin-btn-div-box-2'>
+            <button type='submit' className='signin-btn'>
+              SIGN IN
+            </button>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
