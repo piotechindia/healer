eb17fb008b9b48523db3a9294ec775665fc44417
Coding Hindi <84448185+codinghindi@users.noreply.github.com>
2024-01-02 19:02:30 +0000

    new file:   app/contact/contact.module.css 	modified:   app/contact/page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,18 +1,72 @@
-import Image from 'next/image'
-import Header from '../components/header'
-import Footer from '../components/footer'
-import Sidebar from '../components/sidebar'
-import styles from '../styles/styles.module.css'
+"use client"
+import React, { useState } from 'react';
+import Header from '../components/header';
+import Footer from '../components/footer';
+import Sidebar from '../components/sidebar';
+import styles from '../styles/styles.module.css';
+import Cstyle from './contact.module.css'
 
-export default function Contact() {
+const Contact = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    subject: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const scriptURL = 'https://script.google.com/macros/s/AKfycbxOUeRz7XTfO79gYr4XI79X4W34zkn78YeUmYQziGlPVS_ROSEi1R_B7QZ4dv7-fSla/exec';
+      const response = await fetch(scriptURL, {
+        method: 'POST',
+        body: new FormData(e.target)
+      });
+      if (response.ok) {
+        alert('Your Attendance Has Been Submitted. Thank You!!');
+        setFormData({
+          name: '',
+          email: '',
+          subject: ''
+        });
+      } else {
+        throw new Error('Submission failed');
+      }
+    } catch (error) {
+      console.error('Error!', error.message);
+    }
+  };
+
   return (
     <main className={styles.main}>
-      <Header/>
-      <Sidebar/>
+      <Header />
+      <Sidebar />
       <div className={styles.pageContent}>
-        <p>Contact Page</p>
-        <Footer/>
+        <div className={Cstyle.contactform}>
+          <form onSubmit={handleSubmit} name="google-sheet">
+            <h1 className={Cstyle.heading}>Time to be Healthy and Strong with <br />finding Your inner fitness instinct.</h1>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '20px' }}>
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" name="name" placeholder="Enter Name Here" value={formData.name} onChange={handleChange} required  style={{border: '.1rem solid #ddd', borderRadius: '4px', padding: '10px'}}/>
+              <label htmlFor= "email">Email:</label>
+              <input type="email" id="email" name="email" placeholder="Enter Your Email Here" value={formData.email} onChange={handleChange} required  style={{border: '.1rem solid #ddd', borderRadius: '4px', padding: '10px'}}/ >
+              <label htmlFor="subject">Summary</label>
+              <textarea id="subject" placeholder="write your today's work report here ..." name="subject" value={formData.subject} onChange={handleChange} required style={{border: '.1rem solid #ddd', borderRadius: '4px', padding: '10px',height: '100px'}}></textarea>
+              <button type="submit" className={Cstyle.submitBtn}>Submit Attendance</button>
+            </div>
+          </form>
         </div>
+        <Footer />
+      </div>
     </main>
-  )
-}
+  );
+};
+
+export default Contact;
